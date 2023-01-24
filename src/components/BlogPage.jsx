864a8ca5d5b59864e60f1c393dc35c31af7228dd
Tomoko-hub <haisaitomoco@gmail.com>
2023-01-24 21:14:36 +0200

    portfolio base

---
+++ src/components/BlogPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BlogPage = () => {
+  return (
+    <div className='container'>
+        <h1>Blog</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem commodi alias vel, ut nobis reprehenderit. Minima rem, asperiores suscipit officia placeat nam nobis, odit dolorum veniam quibusdam, eius laborum explicabo! Eligendi recusandae exercitationem magnam illum est explicabo cumque maxime unde dolorem voluptates voluptas, laborum enim commodi fugiat itaque quo sequi mollitia id reprehenderit at, eius dignissimos, asperiores alias ab. Ut maiores id exercitationem aspernatur porro unde rem nemo ea provident fugit, quae dignissimos recusandae natus iure deleniti eius! Illo consequuntur nisi nesciunt vero aliquam provident laboriosam, doloremque consequatur fuga placeat, quidem sint. Sequi veniam quae minima eius, dolorum expedita maiores?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem commodi alias vel, ut nobis reprehenderit. Minima rem, asperiores suscipit officia placeat nam nobis, odit dolorum veniam quibusdam, eius laborum explicabo! Eligendi recusandae exercitationem magnam illum est explicabo cumque maxime unde dolorem voluptates voluptas, laborum enim commodi fugiat itaque quo sequi mollitia id reprehenderit at, eius dignissimos, asperiores alias ab. Ut maiores id exercitationem aspernatur porro unde rem nemo ea provident fugit, quae dignissimos recusandae natus iure deleniti eius! Illo consequuntur nisi nesciunt vero aliquam provident laboriosam, doloremque consequatur fuga placeat, quidem sint. Sequi veniam quae minima eius, dolorum expedita maiores?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem commodi alias vel, ut nobis reprehenderit. Minima rem, asperiores suscipit officia placeat nam nobis, odit dolorum veniam quibusdam, eius laborum explicabo! Eligendi recusandae exercitationem magnam illum est explicabo cumque maxime unde dolorem voluptates voluptas, laborum enim commodi fugiat itaque quo sequi mollitia id reprehenderit at, eius dignissimos, asperiores alias ab. Ut maiores id exercitationem aspernatur porro unde rem nemo ea provident fugit, quae dignissimos recusandae natus iure deleniti eius! Illo consequuntur nisi nesciunt vero aliquam provident laboriosam, doloremque consequatur fuga placeat, quidem sint. Sequi veniam quae minima eius, dolorum expedita maiores?</p>
+    </div>
+  )
+}
+
+export default BlogPage
