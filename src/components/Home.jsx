bd29421ec1e02aa68da1390ab9534fcb5c0a5725
Tomoko-hub <haisaitomoco@gmail.com>
2023-02-08 14:22:13 +0200

    change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -38,7 +38,7 @@ const HomePage = () => {
                             <i className="fas fa-circle fa-stack-2x text-primary"></i>
                             <i className="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
                         </span>
-                        <h4 className="my-3">ECサイト</h4>
+                        <h4 className="my-3">Shopping card with React Redux</h4>
                         <p className="text-muted">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
                             maxime quam architecto quo inventore harum ex magni, dicta
@@ -50,7 +50,7 @@ const HomePage = () => {
                             <i className="fas fa-circle fa-stack-2x text-primary"></i>
                             <i className="fas fa-laptop fa-stack-1x fa-inverse"></i>
                         </span>
-                        <h4 className="my-3">レスポンシブサイト</h4>
+                        <h4 className="my-3">Responsive Site</h4>
                         <p className="text-muted">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
                             maxime quam architecto quo inventore harum ex magni, dicta
@@ -62,7 +62,7 @@ const HomePage = () => {
                             <i className="fas fa-circle fa-stack-2x text-primary"></i>
                             <i className="fas fa-lock fa-stack-1x fa-inverse"></i>
                         </span>
-                        <h4 className="my-3">ウェブセキュリティ</h4>
+                        <h4 className="my-3">Blog</h4>
                         <p className="text-muted">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
                             maxime quam architecto quo inventore harum ex magni, dicta
